f9bd0c626113a98a744cd177c1bc80d9de95a6c7
Joakim Rydh <joakim.rydh@gmail.com>
2025-06-22 23:33:16 +0200

    Add cob folder - copy to vial-qmk/keyboards/

---
+++ cob/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x95, 0x2D, 0x2B, 0x3B, 0x26, 0x9C, 0xDC, 0xBB}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
